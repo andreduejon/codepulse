10d240f233dd926ef87bf81ebab2d950e1e0171f
André Düjon <andre.duejon@lhind.dlh.de>
2026-04-05 20:56:10 +0200

    fix: set process.title to codepulse on startup for consistent terminal tab name

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 #!/usr/bin/env bun
 import "@opentui/solid/preload";
 
+process.title = "codepulse";
+
 const { main } = await import("./main");
 main().catch((err: unknown) => {
   console.error("Fatal error:", err);
